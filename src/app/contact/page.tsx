dfea4607c0fd9c28c9eab32069d443581ae098aa
FareaaFaisal <165368771+FareaaFaisal@users.noreply.github.com>
2025-02-04 13:26:44 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,10 +7,10 @@ export default function Contact(){
         <div className="mt-6">
   <div className="grid sm:grid-cols-2 items-start gap-12 p-8 mx-auto max-w-4xl bg-white shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md font-[sans-serif]">
     <div>
-      <h1 className="text-gray-800 text-3xl font-bold">Let's Talk</h1>
+      <h1 className="text-gray-800 text-3xl font-bold">Let&apos;s Talk</h1>
       <p className="text-sm text-gray-500 mt-4">
         Have some big idea or brand to develop and need help? Then reach out
-        we'd love to hear about your project and provide help.
+        we&apos;d love to hear about your project and provide help.
       </p>
       <div className="mt-12">
         <h2 className="text-gray-800 text-base font-bold">Email</h2>
